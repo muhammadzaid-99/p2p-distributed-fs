1d9b89fd6227825456dfb31a7d98fffb6ec1dd9f
Muhammad Zaid <muhammadzaid00912@gmail.com>
2026-09-02 00:05:31 +0500

    fs

---
+++ server.go
@@ -0,0 +1,35 @@
+package main
+
+import "github.com/muhammadzaid-99/distfs/p2p"
+
+type FileServerOpts struct {
+	ListenAddr        string
+	StorageRoot       string
+	PathTransformFunc PathTransformFunc
+	Transport         p2p.Transport
+}
+
+type FileServer struct {
+	FileServerOpts
+
+	store *Store
+}
+
+func NewFileServer(opts FileServerOpts) *FileServer {
+	storeOpts := StoreOpts{
+		Root:              opts.StorageRoot,
+		PathTransformFunc: opts.PathTransformFunc,
+	}
+	return &FileServer{
+		FileServerOpts: opts,
+		store:          NewStore(storeOpts),
+	}
+}
+
+func (s *FileServerOpts) Start() error {
+	if err := s.Transport.ListenAndAccept(); err != nil {
+		return err
+	}
+
+	return nil
+}
--- store.go
+++ store.go
@@ -109,6 +109,10 @@ func (s *Store) Read(key string) (io.Reader, error) {
 	return buf, err
 }
 
+func (s *Store) Write(key string, r io.Reader) error {
+	return s.writeStream(key, r)
+}
+
 func (s *Store) readStream(key string) (io.ReadCloser, error) {
 	pathKey := s.PathTransformFunc(key)
 	return os.Open(s.fromRoot(pathKey.FullPath()))
